62b34d922fd217bda2e8c1fe2249d2d2f4214fd2
Your Name <mat.holmes@outlook.com>
2024-02-06 08:59:22 +0000

    add user input

--- accounts.py
+++ accounts.py
@@ -1,1 +1,6 @@
 # theis program reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+
+# get user input, saves input as a string 
+account_number = input("Please enter an 10 digit account number: ")
+
+print(account_number)
