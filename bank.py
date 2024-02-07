6b37f747c52d93793670cf4c89493e9a6371f00f
Your Name <mat.holmes@outlook.com>
2024-02-07 11:03:34 +0000

    change all float to int

--- bank.py
+++ bank.py
@@ -3,14 +3,14 @@ amount1 = input("Enter amount1 (in cent): ")
 amount2 = input("Enter amount2 (in cent): ")
 
 # you cant do arithmetic on strings, so convert string inputs to floats
-amount1_float = float(amount1)
-amount2_float = float(amount2)
+amount1_int = int(amount1)
+amount2_int = int(amount2)
 
 # add the two amounts together
-cents = amount1_float + amount2_float
+cents = amount1_int + amount2_int
 
 # convert cents to euros.
-euros = cents / 100.0
+euros = cents / 100
 
 # fstrings seems to be a newer way of formatting outputs
 # note that the euro sign is in Unicode (\u20AC)
