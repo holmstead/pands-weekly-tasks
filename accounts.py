538699741352fd98bfada56478327b7d601014ec
Your Name <mat.holmes@outlook.com>
2024-02-07 00:26:17 +0000

    add print Xs

--- accounts.py
+++ accounts.py
@@ -17,12 +17,20 @@ account_number = input("Please enter an 10 digit account number: ")
 # https://www.askpython.com/python/list/negative-indexing
 
 # print last character of the array
-print(account_number[-1])
+#print(account_number[-1])
 
 # print last 4 characters now
-print(account_number[-4], account_number[-3], account_number[-2], account_number[-1])
+#print(account_number[-4], account_number[-3], account_number[-2], account_number[-1])
 
 # that printed the last 4 numbers but with spaces between, i want to remove spaces
 # found example here: https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
 
-print(account_number[-4], account_number[-3], account_number[-2], account_number[-1], sep="")
+#print(account_number[-4], account_number[-3], account_number[-2], account_number[-1], sep="")
+
+# now print a load of Xs before the last 4 numbers
+# its 6 Xs in this case, but would be better to count the array length and subtract 4 from it
+number_of_xs = len(account_number)-4
+#print('X' * number_of_xs)
+
+# combine the Xs and the last 4 digits
+print('X' * number_of_xs, account_number[-4], account_number[-3], account_number[-2], account_number[-1], sep="")
