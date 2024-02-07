64ad2a5b0ccb8d71ccee3388c0e3b137a55f04c0
Your Name <mat.holmes@outlook.com>
2024-02-07 10:42:53 +0000

    remove str.format output

--- bank.py
+++ bank.py
@@ -12,8 +12,5 @@ cents = amount1_float + amount2_float
 # convert cents to euros.
 euros = cents / 100.0
 
-# print to console. Formatting of the floating point numbers is done using the str.format method as described in pythons official documentation found here: https://docs.python.org/3/tutorial/inputoutput.html
-#print ('The sum of these is \u20AC{:.2f}'.format(float(euros)))
-
 # fstrings seems to be a newer way of formatting outputs
 print(f'The sum of these is \u20AC{float(euros):.2f}')
