27121e0da7887c0e129c85610f119bd3bb13e8cf
Your Name <mat.holmes@outlook.com>
2024-02-07 11:01:57 +0000

    add Unicode info

--- bank.py
+++ bank.py
@@ -13,4 +13,8 @@ cents = amount1_float + amount2_float
 euros = cents / 100.0
 
 # fstrings seems to be a newer way of formatting outputs
+# note that the euro sign is in Unicode (\u20AC)
+# https://docs.python.org/3/howto/unicode.html
+# https://home.unicode.org/
+# take a look at using \N{name} eg \N{euro sign}
 print(f'The sum of these is \u20AC{float(euros):.2f}')
