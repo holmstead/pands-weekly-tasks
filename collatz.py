6e70e9728fd72d874457a38f95aa9904d8d63666
Your Name <mat.holmes@outlook.com>
2024-02-12 19:58:25 +0000

    add user input

--- collatz.py
+++ collatz.py
@@ -1,4 +1,8 @@
 # this program asks the user to input any positive integer and outputs the successive values of the following calculation:
-# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+# the program calculates the next value by taking the current value and, if it is even, divides it by two, but if it is odd, multiplys it by three and adds one
+# the program ends if the current value is one
 
+# get user input, saves input as a string 
+positive_integer = input("Please enter a positive integer: ")
 
+print(positive_integer)
