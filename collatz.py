473aedaf9cd9c792f5bc6d9bb404cfc13d537f7e
Your Name <mat.holmes@outlook.com>
2024-02-13 17:33:39 +0000

    add while loop

--- collatz.py
+++ collatz.py
@@ -3,20 +3,21 @@
 # the program ends if the current value is one
 
 # get user input, saves input as an 'int' type
-positive_integer = int(input("Please enter a positive integer: "))
+number_to_check = int(input("Please enter a positive integer: "))
 
 # the modulo operator can be used to determine if a given integer is even or odd
 # https://www.freecodecamp.org/news/the-python-modulo-operator-what-does-the-symbol-mean-in-python-solved/
 
-if ((positive_integer % 2) == 0):
-    # "positive integer" is divided by 2 and the 
-    # remainder is returned, if its zero then it is
-    # an even number
-    print(f'{positive_integer} is even')
-    was_even = positive_integer/2
-    print(f'divided by 2 = {was_even}')
-else:
-    print(f'{positive_integer} is odd')
-    was_odd = (positive_integer*3) + 1
-    print(f'divided by 2 = {was_odd}')
+while number_to_check != 1:
+    if ((number_to_check % 2) == 0):
+        # "positive integer" is divided by 2 and the 
+        # remainder is returned, if its zero then it is
+        # an even number
+        print(f'{number_to_check} is even')
+        number_to_check = (number_to_check / 2)
+        print(f'divided by 2 = {number_to_check}')
+    else:
+        print(f'{number_to_check} is odd')
+        number_to_check = (number_to_check * 3) + 1
+        print(f'multiplied by 3 and add 1 = {number_to_check}')
 
