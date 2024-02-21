b9ae3967ec69f4972ae1c8a913c597d3ff79c555
Your Name <mat.holmes@outlook.com>
2024-02-21 00:10:15 +0000

    add datetime

--- weekday.py
+++ weekday.py
@@ -1,6 +1,9 @@
 # This program outputs whether or not today is a weekday.
 
+from datetime import datetime
+
 # define a list of the days of the week
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+#print(days)
 
-print(days)
+print(f'Datetime is: {datetime.now()}')
