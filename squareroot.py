52a9858142054d41a415ae548c4fec920c5906a7
Your Name <mat.holmes@outlook.com>
2024-02-27 17:01:53 +0000

    Initial commit

---
+++ squareroot.py
@@ -0,0 +1,12 @@
+# square root function
+
+# define function
+def get_square_root(x):
+    '''
+    takes a positive float and gives approximate 
+    square root
+    '''
+    print(x)
+
+# call teh function
+get_square_root(2.3)
