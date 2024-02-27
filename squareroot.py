ec631c7716a4d40fe24c9bdc395dc53c5d52057f
Your Name <mat.holmes@outlook.com>
2024-02-27 19:02:39 +0000

    add description

--- squareroot.py
+++ squareroot.py
@@ -1,4 +1,9 @@
-# square root function
+'''
+A square root function using Newtons method.
+Newtons method is an iterative method which finds roots of functions. It draws tangent lines and tries to get to zero through iteration.
+
+You have to give it a guess number to start with.
+'''
 
 # define function
 def get_square_root(x):
