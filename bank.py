e499280ffc8351a3ba84ad7711eeb70280c9767a
Your Name <mat.holmes@outlook.com>
2024-02-29 21:23:24 +0000

    add return statement in function

--- bank.py
+++ bank.py
@@ -1,12 +1,12 @@
 '''
-This program prompts the user and reads in two money amounts (in cent), add the two amounts, then prints out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount. 
+This program prompts the user and reads in two money amounts (in cent), adds the two amounts, then prints out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount. 
 '''
 
 # get user input, saves input as a string 
 amount1 = input("Enter amount1 (in cent): ")
 amount2 = input("Enter amount2 (in cent): ")
 
-# create function to add cents and convert to euro
+# create function to add cents then convert to euro
 def add_cents(amount1_int, amount2_int):
     # add the two amounts together
     # convert strings to integers first
@@ -15,20 +15,13 @@ def add_cents(amount1_int, amount2_int):
     # convert cents to euros
     euros = cents / 100
 
-    # print result using fstrings
-    # unicode used for the euro sign
-    print(f'The sum of these is \u20AC{float(euros):.2f}')
+    #print(f'The sum of these is \u20AC{float(euros):.2f}')
+    return euros
 
-add_cents(amount1, amount2)
+#add_cents(amount1, amount2)
 
+# print result using fstrings
+# unicode used for the euro sign
+# call the function inside the print statement
+print(f'The sum of these is \u20AC{float(add_cents(amount1, amount2)):.2f}')
 
-'''
-fstrings seems to be a newer way of formatting outputs.
-Note that the euro sign is in Unicode (\u20AC)
-- https://docs.python.org/3/howto/unicode.html
-- https://home.unicode.org/
-Take a look at using \N{name} eg \N{euro sign}
-Note that euros is rounded to 2 decimal places using .2f
-
-Should add exception handling, in case unexpected data types entered.
-'''
