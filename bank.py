9212e50f5cfe8cc4e8b780d9af8b2f2f2e8528ed
Your Name <mat.holmes@outlook.com>
2024-03-02 12:37:07 +0000

    add exception handling

--- bank.py
+++ bank.py
@@ -2,9 +2,20 @@
 This program prompts the user and reads in two money amounts (in cent), adds the two amounts, then prints out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount. 
 '''
 
+import sys
+
 # get user input, saves input as a string 
 amount1 = input("Enter amount1 (in cent): ")
 amount2 = input("Enter amount2 (in cent): ")
+
+# check if an integer was entered or not
+# called exception handling
+try:
+    int_value = int(amount1) and int(amount2)
+    print("Inputs are an integer")
+except ValueError:
+    print("Please enter valid integers.")
+    sys.exit(1)
 
 # create function to add cents then convert to euro
 def add_cents(amount1_int, amount2_int):
@@ -13,15 +24,14 @@ def add_cents(amount1_int, amount2_int):
     cents = int(amount1) + int(amount2)
 
     # convert cents to euros
-    euros = cents / 100
+    # convert integer to float for decimal places
+    euros = float(cents / 100)
 
-    #print(f'The sum of these is \u20AC{float(euros):.2f}')
     return euros
 
-#add_cents(amount1, amount2)
 
 # print result using fstrings
 # unicode used for the euro sign
 # call the function inside the print statement
-print(f'The sum of these is \u20AC{float(add_cents(amount1, amount2)):.2f}')
+#print(f'The sum of these is \u20AC{add_cents(amount1, amount2)}')
 
