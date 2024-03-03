e8f00173223a16b15821da06e14d1fd30f0da1b8
Your Name <mat.holmes@outlook.com>
2024-03-03 16:19:43 +0000

    added function version of exception handling

--- bank.py
+++ bank.py
@@ -2,11 +2,16 @@
 This program prompts the user and reads in two money amounts (in cent), adds the two amounts, then prints out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount. 
 '''
 
-import sys
-
-# get user input, saves input as a string 
-amount1 = input("Enter amount1 (in cent): ")
-amount2 = input("Enter amount2 (in cent): ")
+# create function to read integer from user, using exception handling 
+def read_num():
+    # initalise the variable, leave empty with False
+    num = False
+    while (num == False):
+        try:
+            num = int(input("Enter amount (in cent): "))
+        except ValueError:
+            print("Please enter a valid integer.", end=" ")
+    return num
 
 '''
 Check if an integer was entered or not.
@@ -19,23 +24,18 @@ ValueError is a built-in exception in python:
 A ValueError is "raised when an operation or function receives an argument that has the right type but an inappropriate value, and the situation is not described by a more precise exception such as IndexError."
 '''
 
-try:
-    int_value = int(amount1) and int(amount2)
-    #print("Inputs are integers")
-except ValueError:
-    print("Please enter valid integers.\nExiting")
-    sys.exit(1)
+# call the function for both inputs
+amount1 = read_num()
+amount2 = read_num()
 
 # create function to add cents then convert to euro
-def add_cents(amount1_int, amount2_int):
+def add_cents(amount1, amount2):
     # add the two amounts together
     # convert strings to integers first
-    cents = int(amount1) + int(amount2)
-
+    cents = amount1 + amount2
     # convert cents to euros
     # convert integer to float for decimal places
     euros = float(cents / 100)
-
     # return a variable containing the converted answer
     return euros
 
