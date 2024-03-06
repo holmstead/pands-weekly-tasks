c7fd7fecf3511dfee6faaf6ff7b16acdad005a26
Your Name <mat.holmes@outlook.com>
2024-03-06 13:07:27 +0000

    add try/except inside if/else method

--- es.py
+++ es.py
@@ -7,35 +7,39 @@ import sys      # used to take arguements in terminal
 # https://docs.python.org/3/library/sys.html
 # we gonna use sys.argv specifically to pass in arguement
 
-
-# Check if two arguements were passed in to python
-# i.e. the script name 'es.py' and the textfile name
-# 'example.txt'
-# https://stackoverflow.com/questions/2626026/python-sys-argv-lists-and-indexes
-
-
+# create function to count instances of a given letter
 def letter_count(inf, letter):
+    # initialise variable to count instances
     letter_count = 0
+    # create for loop to iterate through each line of textfile
     for line in inf:
         # uppercase and lowercase are different characters
         # make everything lowercase to make search easier
         line = line.lower()
-        print(line)
+        #print(line)
         # iterate over each character in each line
         for char in line:
         #print(char)
             # check if the character is e
             if (char == letter):
-                # add to the e_count variable
+                # add to the letter_count variable
                 letter_count += 1
     return letter_count
 
-
-with open(sys.argv[1], "r") as inf:
-    print(letter_count(inf, "e"))
-
-
-
+# Check if two arguements were passed in to python
+# i.e. the script name 'es.py' and the textfile name
+# 'example.txt'
+# https://stackoverflow.com/questions/2626026/python-sys-argv-lists-and-indexes
+if len(sys.argv) < 2:
+    print("Usage: python es.py <filename>\nExiting.")
+    # kill the program if we didnt get 2 args
+    sys.exit(1)
+else:
+    try:
+        with open(sys.argv[1], "r") as inf:
+            print(letter_count(inf, "e"))
+    except FileNotFoundError:
+        print("File not found.\nExiting.", end="\n")
 
 
 '''
