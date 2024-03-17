3e92e7e167813f00047700e106c75ca514768de4
Your Name <mat.holmes@outlook.com>
2024-03-17 14:32:35 +0000

    add plot histogram

--- plottask.py
+++ plottask.py
@@ -5,10 +5,21 @@ This program displays:
 on the one set of axes.
 '''
 
+# shttps://numpy.org/doc/stable/index.html
 from numpy import random
+#
+import matplotlib.pyplot as plt
 
 # genertae an array of size 1000, containing numbers up to 100
 # https://numpy.org/doc/stable/reference/random/generated/numpy.random.rand.html
 rand_array = random.randint(100, size=(1000))
 
-print(rand_array)
+#print(rand_array)
+
+# plot a histogram of the array
+# https://realpython.com/python-matplotlib-guide/
+# https://matplotlib.org/stable/gallery/statistics/hist.html
+plt.hist(rand_array, bins= 10, edgecolor='black')
+ 
+# display teh plot
+plt.show()
