2d8bafc1f9026f666612f28df42c37c168963fb8
Your Name <mat.holmes@outlook.com>
2024-03-17 19:48:49 +0000

    add mean & std check, and plot decorations

--- plottask.py
+++ plottask.py
@@ -16,29 +16,51 @@ import matplotlib.pyplot as plt
 rand_array = random.randint(100, size=(1000))
 #print(rand_array)
 
-# use default_rng() function
-# random number generator to produce useful distributions
-#  - https://numpy.org/doc/stable/reference/random/generator.html
-#  - https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.normal.html#numpy.random.Generator.normal
-# you can give a seed number
+'''
+That was the old 'RandomState' way using randint(), new way is 'Generator'
+    - https://numpy.org/doc/stable/reference/random/generator.html#numpy.random.Generator
+
+Use default_rng() method 'random number generator' to generate a random ffloat which we will then use to produce useful distributions
+    - https://numpy.org/doc/stable/reference/random/generator.html#numpy.random.default_rng
+
+From the docs: "default_rng is the recommended constructor for the random number class Generator"
+    - https://numpy.org/doc/stable/reference/random/generator.html
+
+Returns an "initialized generator object".
+
+You can give a seed number for pseudorandom numbers
+'''
+# use generator for random number, seeded by entropy from the operating system
 rng = np.random.default_rng(seed=None)
+print(rng)
+
+'''
+Next we use normal() method on the "initialized generator object" to get a Gaussian distributiuon of random floats
+ - https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.normal.html#numpy.random.Generator.normal
+'''
+# generate array of size 1000  
 # loc is mean, scale is std deviation 
-rand_array = rng.normal(loc=5, scale=1.0, size=1000)
+#rand_array = rng.normal(loc=5, scale=2.0, size=1000)
 
 # try standard_normal()
-#rand_array = rng.standard_normal(1000)
+rand_array = rng.standard_normal(mean=5, std=2, size=1000)
 
 # print the array to the console
 print(rand_array)
 
 # test if mean and std dev are what i want
-print(rand_array.mean())
+print(f"Mean of distribution:\t\t {rand_array.mean()}")
+print(f"Std deviation of distribution:\t {rand_array.std()}")
 
 # plot a histogram of the array
 #  - https://realpython.com/python-matplotlib-guide/
 #  - https://matplotlib.org/stable/gallery/statistics/hist.html
 plt.hist(rand_array, bins= 10, edgecolor='black')
 
+# decorate the plot
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('Histogram of Random Numbers')
 
 # display teh plot
 plt.show()
