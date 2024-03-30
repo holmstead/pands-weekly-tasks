03b84cf5d3d37be97058fbde6b46a41e6e21c2f4
Your Name <mat.holmes@outlook.com>
2024-03-30 11:11:59 +0000

    add if name == main method

--- weekday.py
+++ weekday.py
@@ -1,10 +1,8 @@
 # This program outputs whether or not today is a weekday.
 
-# import datetime library
+# import datetime library [1]
 from datetime import datetime
 
-# datetime docs: 
-# https://docs.python.org/3/library/datetime.html
 
 # create a function
 def check_if_weekend():
@@ -15,9 +13,7 @@ def check_if_weekend():
     # store day integer in a variable
     i = datetime.now().weekday()
     
-    # print name of day uings strftime() method
-    # https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
-    # https://www.geeksforgeeks.org/python-strftime-function/
+    # print name of day uings strftime() method [2], [3]
     print(f"Today is {datetime.now().strftime('%A')}")
 
     # Sat is 5, Sun is 6. Both are > 4
@@ -29,5 +25,22 @@ def check_if_weekend():
         answer = "Yes, unfortunately today is a weekday."
         return answer
 
-# call the function
-print(check_if_weekend())
+
+# create main() function [4], [5]
+def main():
+    # call the function
+    is_weekend = check_if_weekend()
+    print(is_weekend)
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+[1] https://docs.python.org/3/library/datetime.html
+[2] https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+[3] https://www.geeksforgeeks.org/python-strftime-function/
+[4] https://realpython.com/if-name-main-python/
+[5] https://www.youtube.com/watch?v=g_wlZ9IhbTs&t=103s
+
+'''
