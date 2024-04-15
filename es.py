075cdff586659d021ec574bc100f66befe1a97c3
Your Name <mat.holmes@outlook.com>
2024-04-15 18:56:39 +0100

    move references to README

--- es.py
+++ es.py
@@ -4,8 +4,10 @@
 # e.g. python3 es.py example.txt
 
 import sys      # used to take arguements in terminal
-# https://docs.python.org/3/library/sys.html
-# we gonna use sys.argv specifically to pass in arguement
+
+
+# define variable with the letter to count
+letter = "e"
 
 # create function to count instances of a given letter
 def letter_count(inf, letter):
@@ -28,39 +30,20 @@ def letter_count(inf, letter):
 
 
 # create main() function
-# https://realpython.com/if-name-main-python/
-# https://www.youtube.com/watch?v=g_wlZ9IhbTs&t=103s
 def main():
     # Check if two arguements were passed in to python
-    # i.e. the script name 'es.py' and the textfile name
-    # 'example.txt'
-    # https://stackoverflow.com/questions/2626026/python-sys-argv-lists-and-indexes
+    # i.e. the script name 'es.py' and the textfile name 'example.txt'
     if len(sys.argv) != 2:
         print("Usage: python es.py <filename>\nExiting.")
         # kill the program if we didnt get 2 args
         sys.exit(1)
     else:
         try:
-            with open(sys.argv[1], "r") as inf:     # [1]
-                print(letter_count(inf, "e"))
+            with open(sys.argv[1], "r") as inf:
+                print(letter_count(inf, letter))
         except FileNotFoundError:
             print("File not found.\nExiting.", end="\n")
 
 
 if __name__ == "__main__":
     main()
-
-
-'''
-[1] Open the file (example.txt) in read-only mode
-I'm using the "with open" way as it automatically 
-closes the file after.
-- https://www.freecodecamp.org/news/with-open-in-python-with-statement-syntax-example/
-Lets open the file and use some built-in string methods
-- https://docs.python.org/3/library/stdtypes.html#string-methods
-
-'''
-
-
-
-
