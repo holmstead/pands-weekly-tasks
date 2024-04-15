c2cebbed7a9dd930abcbe78c60f26a12d1f1ab63
Your Name <mat.holmes@outlook.com>
2024-04-15 18:35:57 +0100

    move references to README

--- collatz.py
+++ collatz.py
@@ -18,7 +18,7 @@ while number_to_check == False:
     try:
         number_to_check = int(input("Please enter a positive integer: "))
     except ValueError:
-        print("Not an integer")
+        print("Not an integer.", end= " ")
 
 # start a while loop (sentinel controlled loop) that 
 # will run only if n does not equal one
@@ -26,28 +26,26 @@ while number_to_check != 1:
     # check if the remainder of the number when divided 
     # by two is equal to zero
     if number_to_check % 2 == 0: 
-        # if the remainder is zero then it was
-        # an even number
-        #print(f'{number_to_check} is even')
+        # if the remainder is zero then it was an even number
+        #print(f"{number_to_check} is even")
+        
         # now do the even number math on it
         number_to_check = (number_to_check / 2)
+        
         # print the result of the math to console
-        #print(f'divided by 2 = {number_to_check}')
-        # Use the 'end' parameter of the print function
+        #print(f"divided by 2 = {number_to_check}")
+        
+        # Use the "end" parameter of the print function
         # to print across console
-        print(int(number_to_check), end=' ')
+        print(int(number_to_check), end=" ")
+    
     else:
-        #print(f'{number_to_check} is odd')
+        #print(f"{number_to_check} is odd")
         number_to_check = (number_to_check * 3) + 1
-        #print(f'multiplied by 3 and add 1 = {number_to_check}')
-        # use the 'end' parameter of the print function
+        #print(f"multiplied by 3 and add 1 = {number_to_check}")
+        
+        # use the "end" parameter of the print function
         # to print across console
-        print(int(number_to_check), end=' ')
+        print(int(number_to_check), end=" ")
 
 print()
-
-'''
-The modulo operator can be used to determine if a given integer is even or odd
-    - https://www.freecodecamp.org/news/the-python-modulo-operator-what-does-the-symbol-mean-in-python-solved/
-
-'''
