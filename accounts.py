1146509438456328b8ff6976e20bcaa221fb1951
Your Name <mat.holmes@outlook.com>
2024-04-15 18:11:30 +0100

    move references to README

--- accounts.py
+++ accounts.py
@@ -1,46 +1,25 @@
 '''
-This program reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
+This program reads in a 10 character account number and 
+outputs the account number with only the last 4 digits 
+showing (and the first 6 digits replaced with Xs).
 '''
-
-# get user input, save input as a string 
-account_number = input("Please enter an 10 digit account number: ")
 
 # create function to redact account number
-def redact_acc_number(account_number):
+# defaulted the function to disply 4 last digits
+def redact_number(acc, visible_digits=4):
     # strip spaces from input
-    # string.replace() method replaces whatever is in the first argument with whatevers in the second argument
-    account_number_without_spaces = account_number.replace(" ", "")
-
+    # string.replace() method replaces whatever is in 
+    # the first argument with whatevers in the second
+    # argument
+    acc_without_spaces = acc.replace(" ", "")
     # determine number of X to print
-    number_of_Xs = len(account_number_without_spaces)-4
-
+    number_of_Xs = len(acc_without_spaces) - visible_digits
     # print redacted account number
-    print('X' * number_of_Xs, account_number_without_spaces[-4:], sep="")
+    # use slicing: array[start:stop:step]
+    print('X' * number_of_Xs, acc_without_spaces[-visible_digits::], sep="")
+
+# get user input, save input as a string 
+acc = input("Please enter an 10 digit account number: ")
 
 # call function
-redact_acc_number(account_number)
-
-'''
-"strings are arrays" 
-    - https://www.w3schools.com/python/python_strings.asp
-
-Printed last 4 characters uses splicing:
-    - https://www.freecodecamp.org/news/python-slicing-how-to-slice-an-array/
-    
-    array[start:stop:step]
-
-Array can be navigated using indices
-    Arrays start at zero in python
-    The below code prints the first character of the array
-        print(account_number[0])
-
-How to print last character of an array? negative indexing
- - https://www.askpython.com/python/list/negative-indexing
-
-Removed space between Xs and the 4 numbers using sep=""
-Example found here: 
-    - https://www.geeksforgeeks.org/gfact-50-python-end-parameter-in-print/
-
-    print('X' * number_of_Xs, account_number[-4:], sep="")
-    
-'''
+redact_number(acc)
